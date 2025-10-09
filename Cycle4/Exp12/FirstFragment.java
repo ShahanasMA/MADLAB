69caaa276c4d24840a1f029333eda51c37e92b79
ShahanasMA <chn24mc052@ceconline.edu>
2025-10-09 15:06:04 +0530

    Create FirstFragment.java

---
+++ Cycle4/Exp12/FirstFragment.java
@@ -0,0 +1,31 @@
+package com.example.P11;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FirstFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FirstFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first,container,false);
+    }
+
+}
+
